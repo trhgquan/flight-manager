b4900d61c905192edbcaa03b0782983c5b3b30f9
trhgquan <quanthcstpbt@gmail.com>
2022-05-25 20:44:33 +0700

    Refactor navbar

--- FlightManager/main/urls.py
+++ FlightManager/main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('customer/', views.customer),
 
     # Authentication
-    path('signup/', views.signup),
-    path('login/', views.login),
+    path('signup/', views.signup, name='auth.signup'),
+    path('login/', views.login, name='auth.login'),
 ]
