382cad975af51a3239ef12502636aa15da167793
trhgquan <quanthcstpbt@gmail.com>
2022-05-25 22:56:56 +0700

    Finished frontends: - Sign up & sign in - Create report - Customer detail

--- FlightManager/main/urls.py
+++ FlightManager/main/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     # Authentication
     path('signup/', views.signup, name='auth.signup'),
     path('login/', views.login, name='auth.login'),
+
+    # Report
+    path('report/', views.report, name='report'),
 ]
--- FlightManager/main/views.py
+++ FlightManager/main/views.py
@@ -7,18 +7,23 @@ from django.http import HttpResponse
 def signup(request):
     # More processing for POST method here.
 
-    return render(request, 'accounts/signup.html')
+    return render(request, 'main/signup.html')
 
 def login(request):
     # More processing for POST method here.
 
-    return render(request, 'accounts/login.html')
+    return render(request, 'main/login.html')
 
 def home(request):
-    return render(request, 'accounts/dashboard/dashboard.html')
+    return render(request, 'main/dashboard/dashboard.html')
 
 def flights(request):
-    return render(request, 'accounts/flights.html')
+    return render(request, 'main/flights.html')
 
 def customer(request):
-    return render(request, 'accounts/customer.html')
+    return render(request, 'main/customer.html')
+
+def report(request):
+    # More HTTP POST processing here
+
+    return render(request, 'main/report.html')
