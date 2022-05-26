d5ea2354f23858157a19efca364719b07cc8d370
Quan, Tran Hoang <quanthcstpbt@gmail.com>
2022-05-26 17:16:28 +0700

    Removed conflicts inside views.py

--- FlightManager/main/views.py
+++ FlightManager/main/views.py
@@ -8,14 +8,30 @@ from .models import Airport
 
 # Create your views here.
 
+# Authenticate
+def signup(request):
+    # More processing for POST method here.
+
+    return render(request, 'main/signup.html')
+
+def login(request):
+    # More processing for POST method here.
+
+    return render(request, 'main/login.html')
+
 def home(request):
-    return render(request, 'accounts/dashboard/dashboard.html', {'nbar': 'home'})
+    return render(request, 'main/dashboard/dashboard.html')
 
 def flights(request):
-    return render(request, 'accounts/flights.html')
+    return render(request, 'main/flights.html')
 
 def customer(request):
-    return render(request, 'accounts/customer.html')
+    return render(request, 'main/customer.html')
+
+def report(request):
+    # More HTTP POST processing here
+
+    return render(request, 'main/report.html')
 
 def airport_list(request):
     list = Airport.objects.all()
