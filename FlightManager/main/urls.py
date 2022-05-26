a533ac52aa9d9e0b6d4afb0b9809c51d7ce4a277
nuno314 <tinle1234vt@gmail.com>
2022-05-26 12:24:35 +0700

    add Airport List

--- FlightManager/main/urls.py
+++ FlightManager/main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('flights/', views.flights),
-    path('customer/', views.customer)
+    path('', views.home, name='home'),
+    path('flights/', views.flights, name='flights'),
+    path('customer/', views.customer, name='customer'),
+    path('airport/list/', views.airport_list, name='airport_list')
 ]
--- FlightManager/main/views.py
+++ FlightManager/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Airport
+
 # Create your views here.
 
 def home(request):
@@ -11,3 +13,8 @@ def flights(request):
 
 def customer(request):
     return render(request, 'accounts/customer.html')
+
+def airport_list(request):
+    list = Airport.objects.all()
+    print(list)
+    return render(request, 'airport/airport_list.html', {'airports':list})
